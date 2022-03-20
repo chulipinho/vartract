6edadd0dfa8ca51853c830a3d123ed7fab966f5c
Fellipe Luz Souza Machado <fellipe.luz.machado@gmail.com>
2022-03-20 11:08:51 -0300

    Implement button on edit-page

--- src/Pages/edit-page/index.js
+++ src/Pages/edit-page/index.js
@@ -3,8 +3,11 @@ import styled from "styled-components";
 import { EditPageBody } from "../../Assets/styles/EditPageBody";
 import { TextInputField } from "../../Components/TextInputField";
 import { MainText, Title } from "../../Assets/styles/TextStyles";
+import { ButtonComponent } from "../../Components/Button";
 
 const testList = ['Nome', 'Data', 'Nome2', 'idade', 'quantidade'];
+const LANGUAGE = 'en';
+const appText = require(`../../Assets/text/app-texts-${LANGUAGE}.json`);
 
 const FormatedDiv = styled.div`
     padding: 0 50px;
@@ -17,13 +20,14 @@ export class EditPage extends React.Component {
         return (
             <EditPageBody>
                 <FormatedDiv>
-                    <Title>teste</Title>
+                    <Title>{appText["edit-page"].title}</Title>
                     <MainText>
-                        Please, fill the form fields acordingly then press the submit button.
+                        {appText["edit-page"].instructions}
                     </MainText>
                     <form>
                         {testList.map(e => <TextInputField key={e} name={e} />)}
                     </form>
+                    <ButtonComponent>{appText["edit-page"].button}</ButtonComponent>
                 </FormatedDiv>
             </EditPageBody>
         );
