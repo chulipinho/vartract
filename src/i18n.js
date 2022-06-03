ee77ab3382c22b87887bd271549f2ade67089659
Fellipe Luz Souza Machado <fellipe.luz.machado@gmail.com>
2022-06-03 11:54:40 -0300

    Add language detection

--- src/i18n.js
+++ src/i18n.js
@@ -1,4 +1,5 @@
 import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
 // @ts-ignore
@@ -11,15 +12,20 @@ const PT_BR_LAGUAGE_PACK = require("./Assets/text/app-texts-pt-br.json");
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 const resources = {
     en: EN_LAGUAGE_PACK,
-    ptbr: PT_BR_LAGUAGE_PACK,
+    "pt-BR": PT_BR_LAGUAGE_PACK,
+    pt: PT_BR_LAGUAGE_PACK,
+};
+
+const DETECTION_OPTIONS = {
+    order: ["navigator"],
 };
 
 i18n.use(initReactI18next) // passes i18n down to react-i18next
+    .use(LanguageDetector)
     .init({
+        detection: DETECTION_OPTIONS,
         resources,
-        lng: "ptbr", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-        // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-        // if you're using a language detector, do not define the lng option
+        fallbackLng: "en",
 
         interpolation: {
             escapeValue: false, // react already safes from xss
